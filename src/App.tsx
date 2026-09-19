064450269d2b49deb2742d23a0f710900e4dfa09
VietsRepo <perprikey@gmail.com>
2026-09-19 15:46:42 +0700

    add project

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="center">
+        <img
+          src={reactLogo}
+          className="framework"
+          alt="React logo"
+          width="50"
+        />
+        <h1>Get started</h1>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
